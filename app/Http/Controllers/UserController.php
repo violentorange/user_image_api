2a957f2cc9a1f77bc452b968e3786f6d5628d253
lorincz.gergely.tamas <lorincz.gergely.tamas@students.jedlik.eu>
2024-05-28 16:06:33 +0200

    init

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request)
+    {
+        if ($request->hasFile('image')) {
+            $image_url = $request->file('image')->store('images', 'public');
+            $request->merge(['image_url' => $image_url]);
+        }
+
+        $user = User::create($request->all());
+
+        return response()->json([
+           'message' => 'User created successfully',
+            'data' => $user,
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
